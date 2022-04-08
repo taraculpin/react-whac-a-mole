5a89eec03e58268ce2ed79dd5413337952c1c51e
Tara Culpin <taraculpin@gmail.com>
2022-04-08 11:48:34 +0100

    added a timer that counts down in 1000ms intervals from 30

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React, { Fragment, StrictMode, useState } from 'react';
+import React, { Fragment, StrictMode, useState, useEffect, useRef } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 // import App from './App';
@@ -6,8 +6,27 @@ import reportWebVitals from './reportWebVitals';
 
 const Moles = ({ children }) => <div>{children}</div>
 const Mole = () => <button>Mole</button>
-const Timer = () => <div>Time: 00:00</div>
+
+const TIME_LIMIT = 30000
+
+const Timer = ({ time, interval = 1000, onEnd }) => {
+  const [internalTime, setInternalTime] = useState(time)
+  const timerRef = useRef(time)
+  const timeRef = useRef(time)
+  useEffect(() => {
+    timerRef.current = setInterval(
+      () => setInternalTime((timeRef.current -= interval)),
+      interval
+    )
+    return () => {
+      clearInterval(timerRef.current)
+    }
+  }, [interval])
+  return <div>{`Time: ${internalTime / 1000}s`}</div>
+}
 const Score = () => <div>Score: 0</div>
+
+
 
 const Game = () => {
   const [playing, setPlaying] = useState(false)
@@ -20,7 +39,7 @@ const Game = () => {
       {playing && (
         <Fragment>
           <Score />
-          <Timer />
+          <Timer time={TIME_LIMIT} onEnd={() => setPlaying(false)}/>
           <Moles>
             <Mole />
             <Mole />
