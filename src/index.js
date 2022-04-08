21d05a9eac4ae33f0efaf57fca72b6aea27039cd
Tara Culpin <taraculpin@gmail.com>
2022-04-08 10:01:12 +0100

    Created basis for the game and added a start/stop button

--- src/index.js
+++ src/index.js
@@ -1,13 +1,44 @@
-import React from 'react';
+import React, { Fragment, StrictMode, useState } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+const Moles = ({ children }) => <div>{children}</div>
+const Mole = () => <button>Mole</button>
+const Timer = () => <div>Time: 00:00</div>
+const Score = () => <div>Score: 0</div>
+
+const Game = () => {
+  const [playing, setPlaying] = useState(false)
+  return (
+    <Fragment>
+      {!playing && <h1>Whac-A-Mole</h1>}
+      <button onClick={() => setPlaying(!playing)}>
+        {playing ? 'Stop' : 'Start'}
+      </button>
+      {playing && (
+        <Fragment>
+          <Score />
+          <Timer />
+          <Moles>
+            <Mole />
+            <Mole />
+            <Mole />
+            <Mole />
+            <Mole />
+          </Moles>
+        </Fragment>
+      )}
+    </Fragment>
+  )
+}
+
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <StrictMode>
+    <Game />
+  </StrictMode>,
   document.getElementById('root')
 );
 
