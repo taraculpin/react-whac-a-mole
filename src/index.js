6ea8a528ac4da5d79f53de8d342d1693fbdc4061
Tara Culpin <taraculpin@gmail.com>
2022-04-11 17:43:50 +0100

    Added CSS and basic game complete

--- src/index.js
+++ src/index.js
@@ -3,8 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 // import App from './App';
 import reportWebVitals from './reportWebVitals';
-
-const TIME_LIMIT = 30000
+import gsap from 'gsap';
 
 const Timer = ({ time, interval = 1000, onEnd }) => {
   const [internalTime, setInternalTime] = useState(time)
@@ -26,13 +25,77 @@ const Timer = ({ time, interval = 1000, onEnd }) => {
   }, [interval])
   return <div>{`Time: ${internalTime / 1000}s`}</div>
 }
+const TIME_LIMIT = 30000
 const MOLE_SCORE = 100
 const NUMBER_OF_MOLES = 5
+const POINTS_MULTIPLIER = 0.9
+const TIME_MULTIPLIER = 1.25
 
-const Mole = ({ onWhack }) => (
-  <button onClick={() => onWhack(MOLE_SCORE)}>Mole</button>
-)
-const Moles = ({ children }) => <div>{children}</div>
+const Mole = ({ onWhack, points, delay, speed, pointsMin = 10 }) => {
+  const [whacked, setWhacked] = useState(false)
+  const bobRef = useRef(null)
+  const pointsRef = useRef(points)
+  const buttonRef = useRef(null)
+  useEffect(() => {
+    gsap.set(buttonRef.current, {
+      yPercent: 100,
+      display: 'block'
+    })
+    bobRef.current = gsap.to(buttonRef.current, {
+      yPercent: 0,
+      duration: speed,
+      yoyo: true,
+      repeat: -1,
+      delay: delay,
+      repeatDelay: delay,
+      onRepeat: () => {
+        pointsRef.current = Math.floor(
+          Math.max(pointsRef.current * POINTS_MULTIPLIER, pointsMin)
+        )
+      },
+    })
+    return () => {
+      if (bobRef.current) bobRef.current.kill()
+    }
+  }, [pointsMin, delay, speed])
+
+
+  useEffect(() => {
+    if (whacked) {
+      pointsRef.current = points
+      bobRef.current.pause()
+      gsap.to(buttonRef.current, {
+        yPercent: 100,
+        duration: 0.1,
+        onComplete: () => {
+          gsap.delayedCall(gsap.utils.random(1, 3), () => {
+            setWhacked(false)
+            bobRef.current
+             .restart()
+             .timeScale(bobRef.current.timeScale() * TIME_MULTIPLIER)
+          })
+        },
+      })
+    }
+  }, [whacked])
+
+  const whack = () => {
+    setWhacked(true)
+    onWhack(pointsRef.current)
+  }
+  return (
+    <div className="mole-hole">
+      <button
+        className="mole"
+        ref={buttonRef}
+        onClick={whack}
+      >
+        Mole
+      </button>
+    </div>
+  )
+}
+const Moles = ({ children }) => <div className="moles">{children}</div>
 const Score = ({ value }) => <div>{`Score: ${value}`}</div>
 
 
@@ -74,7 +137,7 @@ const Game = () => {
           <Timer time={TIME_LIMIT} onEnd={endGame}/>
           <Moles>
             {new Array(NUMBER_OF_MOLES).fill().map((_, id) =>
-              <Mole key={id} onWhack={onWhack}/>
+              <Mole key={id} onWhack={onWhack} points={MOLE_SCORE} delay={0} speed={1.5}/>
             )}
           </Moles>
         </Fragment>
